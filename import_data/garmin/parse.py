ff3abe6d3d07cff583b42b84e3cdc5b78edfc488
carolinux <carolinegr@gmail.com>
2020-06-04 12:58:09 +0200

    Use minimum heart rate for Garmin (one value per day)

--- import_data/garmin/parse.py
+++ import_data/garmin/parse.py
@@ -12,12 +12,14 @@ def user_map_to_timeseries(user_map):
         #print(dailies)
         start_epoch = dailies['startTimeInSeconds']
         start_dt = datetime.utcfromtimestamp(start_epoch)
-        for offset, hr in dailies['timeOffsetHeartRateSamples'].items():
-            offset = int(offset)
-            rec1 = (datetime.utcfromtimestamp(start_epoch + offset), hr)
+        #for offset, hr in dailies['timeOffsetHeartRateSamples'].items():
+        #    offset = int(offset)
+        #    rec1 = (datetime.utcfromtimestamp(start_epoch + offset), hr)
             #rec2 = (datetime.utcfromtimestamp(start_epoch + offset + HEART_RATE_SAMPLE_DURATION_SECS), hr)
-            recs.append(rec1)
+            #recs.append(rec1)
             #recs.append(rec2)
+        recs.append((start_dt, dailies['minHeartRateInBeatsPerMinute']))
     s = pd.Series(data=[r[1] for r in recs], index=[r[0] for r in recs])
     s = s.loc[~s.index.duplicated(keep='first')] # sometimes we have a few duplicate entries over the days
     return s.sort_index()
+
