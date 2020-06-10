a4fb8021b2d7130f8f29e5842e370cb6f89a6b91
carolinux <carolinegr@gmail.com>
2020-06-10 13:14:45 +0200

    Add googlefit update to update_data_imports

--- import_data/management/commands/update_data_imports.py
+++ import_data/management/commands/update_data_imports.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
-from import_data.models import OuraMember, FitbitMember
-from retrospective.tasks import update_fitbit_data, update_oura_data
+from import_data.models import OuraMember, FitbitMember, GoogleFitMember
+from retrospective.tasks import update_fitbit_data, update_oura_data, update_googlefit_data
 import time
 import requests
 
@@ -22,4 +22,10 @@ class Command(BaseCommand):
         for f in fitbit_users:
             update_fitbit_data.delay(f.id)
             print("submitted fitbit update for {}".format(f.id))
+            time.sleep(2)
+
+        gf_users = GoogleFitMember.objects.all()
+        for g in gf_users:
+            update_googlefit_data.delay(g.id)
+            print("submitted googlefit update for {}".format(f.id))
             time.sleep(2)
